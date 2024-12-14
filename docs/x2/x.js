99194ee2583b68100f9313d08af08d9712e882b1
keita-sugiura <kei0221tasugi@gmail.com>
2024-12-14 15:34:49 +0900

    一人称視点　WASD

--- docs/x2/x.js
+++ docs/x2/x.js
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 const width = window.innerWidth, height = window.innerHeight;
 
 // camera //////////////////////////////
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 20);
 camera.position.set(1,1,1);
 //camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
@@ -21,7 +22,12 @@ const addOne = (color, x, y, z) => {
     mesh.renderOrder=0;
     scene.add(mesh);
 }
-addOne(0x00ff00, 1, 1, 1);
+addOne(0x0000ff, 1, 1, 1);
+
+var map1 =new THREE.TextureLoader().load('sougen.jpg' ); 
+const ground=new THREE.Mesh(new THREE.PlaneGeometry(10,10),new THREE.MeshPhongMaterial( { map: map1 } ));
+scene.add(ground);
+ground.rotation.x = -Math.PI / 2;
 
 // axes   //////////////////////////////
 const axes=new THREE.AxesHelper();
@@ -44,19 +50,25 @@ const gui = new GUI();
 gui.add(camera.position, 'x', -10, 10);
 
 // keyboard  //////////////////////////////
+let pressW=0,pressS=0,pressD=0,pressA=0;
 document.addEventListener('keydown', (e) => {
-    console.log('code:' + e.code + ', key:' + e.key);
-    let x=0,y=0,z=0;
-    if(e.code=='KeyW')z++;
-    if(e.code=='KeyS')z--;
-    if(e.code=='KeyA')x--;
-    if(e.code=='KeyD')x++;
-    if(e.code=='Space')y++;
-    if(e.shiftKey==true)y--;
-    const uni=0.1;
-    camera.position.x+=x*uni;
-    camera.position.z+=z*uni;
-    camera.position.y+=y*uni;
+    //console.log('code:' + e.code + ', key:' + e.key);
+    if(e.code=='KeyX'){
+        if(pointer.isLocked)pointer.unlock();
+        else pointer.lock();
+        return;
+    }
+    if(e.code=='KeyW')pressW=1;
+    if(e.code=='KeyS')pressS=1;
+    if(e.code=='KeyD')pressD=1;
+    if(e.code=='KeyA')pressA=1;
+});
+
+document.addEventListener('keyup',(e)=>{
+    if(e.code=='KeyW')pressW=0;
+    if(e.code=='KeyS')pressS=0;
+    if(e.code=='KeyD')pressD=0;
+    if(e.code=='KeyA')pressA=0;
 });
 
 // render ///////////////////////////////////
@@ -64,13 +76,21 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
+
+// FPS
+const pointer = new PointerLockControls(camera,renderer.domElement);
+let moveSpeed=0.06;
+
 function animate(time) {
 
     //mesh.rotation.x = time / 2000;
     //mesh.rotation.y = time / 1000;
     //mesh.scale.set(0.2,1,1);
     //camera.position.x = (time%1000) / 1000;
-
+    let forward=pressW-pressS;
+    let right=pressD-pressA;
+    pointer.moveForward(forward*moveSpeed);
+    pointer.moveRight(right*moveSpeed);
     renderer.render(scene, camera);
 
 }
