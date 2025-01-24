fd7175d3ad1c21db3d9b7b263c76fbefa3d05c75
keita-sugiura <kei0221tasugi@gmail.com>
2025-01-24 19:15:34 +0900

    キューブをクリックで削除　

--- docs/x2/x.js
+++ docs/x2/x.js
@@ -8,31 +8,57 @@ const width = window.innerWidth, height = window.innerHeight;
 
 // scene  &  camera //////////////////////////////
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 20);
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 50);
 scene.add(camera);
 scene.background = new THREE.Color().setHex(0x22bb77);
+const boxScene = new THREE.Scene();
+scene.add(boxScene);
+camera.position.set(12, 12, 12);
+camera.lookAt(0, 0, 0);
+
+// light   //////////////////////////////
+const addL = (x, y, z) => {
+    const light = new THREE.DirectionalLight(0xFFFFFF, 3);
+    light.position.set(x, y, z);
+    scene.add(light);
+    return light
+};
+const light1 = addL(-20, 20, 20);
 
 // object //////////////////////////////
 
+var map1 = new THREE.TextureLoader().load('steel.jpg');
 
+// オブジェクトを一度作成してシーンに追加
+const blockMeshes = Array.from({ length: 10 }, (_, x) => 
+    Array.from({ length: 10 }, (_, y) => 
+        Array.from({ length: 10 }, (_, z) => {
+            const geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
+            const material =new THREE.MeshPhongMaterial({ map: map1 })
+            const mesh = new THREE.Mesh(geometry, material);
+            mesh.position.set(x+0.05, y+0.05, z+0.05);
+            mesh.visible = false; // 初期状態では非表示
+            boxScene.add(mesh);
+            return mesh;
+        })
+    )
+);
 
-const addOne = (color, x, y, z) => {
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    const material = new THREE.MeshPhongMaterial({ color: color });
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.scale.set(x, y, z);
-    mesh.renderOrder = 1;
-    //scene.add(mesh);
-    return mesh;
+// オブジェクトの表示・非表示を切り替える関数
+const toggleBlock = (x, y, z) => {
+    const mesh = blockMeshes[x][y][z];
+    mesh.visible = !mesh.visible;
 }
-const cubes = [];
-for(let i = 0; i < 50; i++){
-    let obj = addOne(0x0000ff, 1, 1, 1);
-    scene.add(obj);
-    const genRand=(l,r)=>l+(r-l)*Math.random();
-    obj.position.set(genRand(-5,5),genRand(0,8),genRand(-5,5));
-    cubes.push(obj);
+for(let i=0;i<10;i++){
+    for(let j=0;j<10;j++){
+       for(let k=0;k<10;k++){
+           if(Math.random()<0.2){
+               toggleBlock(i,j,k);
+           }
+       }
+    }
 }
+
 { // crosshair
     const geometry = new THREE.RingGeometry(0, 0.001, 10);
     const material = new THREE.MeshBasicMaterial({ color: 0x555555 });
@@ -44,16 +70,20 @@ for(let i = 0; i < 50; i++){
 
 var map1 = new THREE.TextureLoader().load('sougen.jpg');
 const ground = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), new THREE.MeshPhongMaterial({ map: map1 }));
-scene.add(ground);
+//scene.add(ground);
 ground.rotation.x = -Math.PI / 2;
+ground.position.set(5, 0, 5); // 平面の位置を調整
 
 
-// フォントを読み込み、テキストを作成
+// 文字  //////////////////////////////
 const loader = new FontLoader();
+let message = 'press x and drag to look around \n';
+message+= 'press w,a,s,d,space,shift to move \n';
+message+= 'click to toggle block';
 loader.load('font.json', function (font) {
-    const textGeometry = new TextGeometry('press x to control the view', {
+    const textGeometry = new TextGeometry(message, {
         font: font,
-        size: 0.06,
+        size: 0.1,
         height: 0.001,
         curveSegments: 12,
         bevelEnabled: true,
@@ -62,9 +92,9 @@ loader.load('font.json', function (font) {
         bevelOffset: 0,
         bevelSegments: 5
     });
-    const textMaterial = new THREE.MeshPhongMaterial({ color: "#1D1B7B" });
+    const textMaterial = new THREE.MeshPhongMaterial({ color: "#FF0000" });
     const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.set(0.05, 0.1, 0); // テキストの位置を設定
+    textMesh.position.set(10,11.5,10); // テキストの位置を設定
     scene.add(textMesh);
 });
 
@@ -73,20 +103,10 @@ loader.load('font.json', function (font) {
 const axes = new THREE.AxesHelper();
 axes.material.depthTest = false;
 axes.renderOrder = 10;
-scene.add(axes);
-
-// light   //////////////////////////////
-const addL = (x, y, z) => {
-    const light = new THREE.DirectionalLight(0xFFFFFF, 3);
-    light.position.set(x, y, z);
-    scene.add(light);
-    return light
-};
-const light1 = addL(-1, 2, 4);
-
+//scene.add(axes);
 
 // keyboard  //////////////////////////////
-let pressW = 0, pressS = 0, pressD = 0, pressA = 0;
+let pressW = 0, pressS = 0, pressD = 0, pressA = 0, pressSpace = 0, pressShift = 0;
 document.addEventListener('keydown', (e) => {
     //console.log('code:' + e.code + ', key:' + e.key);
     if (e.code == 'KeyX') {
@@ -94,7 +114,8 @@ document.addEventListener('keydown', (e) => {
         else pointer.lock();
         return;
     }
-    if (e.code == 'Space') jump();
+    if (e.code == 'Space') pressSpace = 1;
+    if (e.code == 'ShiftLeft') pressShift = 1;
     if (e.code == 'KeyW') pressW = 1;
     if (e.code == 'KeyS') pressS = 1;
     if (e.code == 'KeyD') pressD = 1;
@@ -102,6 +123,8 @@ document.addEventListener('keydown', (e) => {
 });
 
 document.addEventListener('keyup', (e) => {
+    if(e.code == 'Space') pressSpace = 0;
+    if(e.code == 'ShiftLeft') pressShift = 0;
     if (e.code == 'KeyW') pressW = 0;
     if (e.code == 'KeyS') pressS = 0;
     if (e.code == 'KeyD') pressD = 0;
@@ -111,29 +134,18 @@ document.addEventListener('keyup', (e) => {
 // レイキャスターを作成
 const raycaster = new THREE.Raycaster();
 
-// 赤色の円を描画する関数
-function drawCircleAtIntersection(point) {
-    const geometry = new THREE.CircleGeometry(0.1, 32);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    const circle = new THREE.Mesh(geometry, material);
-    circle.position.copy(point);
-    circle.rotation.x = -Math.PI / 2; // 地面に平行にする
-    scene.add(circle);
-}
-
-// 右クリックイベントをリッスン
+// 左クリックイベントをリッスン
 document.addEventListener('mousedown', (event) => {
     event.preventDefault();
 
     // カメラの視線方向にレイキャストを設定
     raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
 
-    // 地面との交点を計算
-    const intersects = raycaster.intersectObject(ground);
+    //　最初にヒットしたブロックを取得
+    const intersects = raycaster.intersectObjects(blockMeshes.flat(2)).filter(intersect => intersect.object.visible);
     if (intersects.length > 0) {
-        const intersectionPoint = intersects[0].point;
-        intersectionPoint.y += 0.01; // 少し浮かせる
-        drawCircleAtIntersection(intersectionPoint);
+        const intersectedObject = intersects[0].object;
+        intersectedObject.visible = !intersectedObject.visible;
     }
 });
 
@@ -143,24 +155,7 @@ renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-// FPS
-const pointer = new PointerLockControls(camera, renderer.domElement);
-let moveSpeed = 0.03;
 
-// jump
-let baseHeight = 0.5;
-const smalldif = 0.00001;
-camera.position.set(1, baseHeight, 1);
-camera.lookAt(0, 0, 0);
-let speedY = 0;
-function jump() {
-    speedY = 0.1;
-}
-
-// baseHeightをオブジェクトのプロパティとして定義
-const params = {
-    baseHeight: 0.5
-};
 
 // GUI   ///////////////////
 const gui = new GUI();
@@ -168,25 +163,19 @@ const groundFolder = gui.addFolder('Ground Scale');
 groundFolder.add(ground.scale, 'x', 1, 10).name('Scale').onChange((value) => {
     ground.scale.set(value, value, value);
 });
-groundFolder.open();
 
-// baseHeightのコントロールを追加
-gui.add(params, 'baseHeight', 0.1, 2).name('Base Height').onChange((value) => {
-    camera.position.y = value;
-});
+// FPS
+const pointer = new PointerLockControls(camera, renderer.domElement);
+let moveSpeed = 0.1;
 
 function animate(time) {
 
     let forward = pressW - pressS;
     let right = pressD - pressA;
+    let up = pressSpace - pressShift;
     pointer.moveForward(forward * moveSpeed);
     pointer.moveRight(right * moveSpeed);
-
-    if (camera.position.y > baseHeight - smalldif) {
-        camera.position.y += speedY;
-        camera.position.y = Math.max(camera.position.y, params.baseHeight);
-        speedY -= 0.005;
-    }
+    camera.position.y += up * moveSpeed;
 
     renderer.render(scene, camera);
 
