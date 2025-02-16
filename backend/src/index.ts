e1c59e295e9146ec92d11a2258dcfd3c259607ab
keita-sugiura <kei0221tasugi@gmail.com>
2025-02-16 19:33:16 +0900

    色変更　ユーザーネーム

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,36 +12,54 @@ app.get('/', (c) => {
   return c.text('Hello Hono!', 202);
 })
 
-app.get('/todos', async(c) =>  {
-  const todoList = await c.env.todos.get('todos', 'json') as string[] || [];
-  return c.json(todoList);
-})
+const colorCount = 3;
 
-app.post('/todos', async (c) => {
-  const body = await c.req.json();
-  let todoList: string[] = [];
-  const temp = await c.env.todos.get('todos', 'json');
-  if(temp){
-    todoList = temp as string[];
+type RequestJson ={
+  todoList: string[],
+  colorList: number[]
+}
+app.get('/todos/:user', async(c) =>  {
+  const user = c.req.param('user');
+  const requestJson = await c.env.todos.get(user, 'json');
+  if(requestJson){
+    return c.json(requestJson as RequestJson);
+  }else{
+    return c.json({todoList: [], colorList: []});
   }
-  todoList.push(body.todo);
-  await c.env.todos.put('todos', JSON.stringify(todoList));
-  return c.json(todoList);
 })
 
-app.delete('/todos/:index', async(c) => {
+app.post('/todos/:user', async (c) => {
+  const user = c.req.param('user');
+  const body = await c.req.json();
+  let json: RequestJson = {todoList: [], colorList: []};
+  const temp = await c.env.todos.get(user,'json') as RequestJson;
+  if(temp){
+    json = temp;
+  }
+  json.todoList.push(body.todo);
+  json.colorList.push(0);
+  await c.env.todos.put(user, JSON.stringify(json));
+  return c.json(json);
+})
+
+app.put('/todos/:user/:index', async (c) => {
   const index = parseInt(c.req.param('index'), 10);
-  const todoList = await c.env.todos.get('todos', 'json') as string[] || [];
-  todoList.splice(index, 1);
-  await c.env.todos.put('todos', JSON.stringify(todoList));
-  return c.json(todoList);
+  const user = c.req.param('user');
+  const json = await c.env.todos.get(user, 'json') as RequestJson;
+  json.colorList[index] = (json.colorList[index] + 1) % colorCount;
+  await c.env.todos.put(user, JSON.stringify(json));
+  return c.json(json);
 })
 
-app.get('/todos/:id/:name', (c) => {
-  const p = c.req.query("query");
-  const id = c.req.param('id');
-  const name = c.req.param('name');
-  return c.json({ id, name, p });
+app.delete('/todos/:user/:index', async (c) => {
+  const index = parseInt(c.req.param('index'), 10);
+  const user = c.req.param('user');
+  const json = await c.env.todos.get(user, 'json') as RequestJson;
+  json.todoList.splice(index, 1);
+  json.colorList.splice(index, 1);
+  await c.env.todos.put(user, JSON.stringify(json));
+  return c.json(json);
 })
+
 
 export default app
--- docs/vite/src/App.tsx
+++ docs/vite/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useEffect} from 'react'
+import { useEffect } from 'react'
 import axios from 'axios';
 import './App.css'
 
@@ -12,25 +12,35 @@ import IconButton from '@mui/material/IconButton';
 import CheckIcon from '@mui/icons-material/Check';
 
 
-const url= 'https://backend.kei0221tasugi.workers.dev/todos';
+const url = 'https://backend.kei0221tasugi.workers.dev/todos';
 
 function App() {
-  const [text, setText] = useState('hello');
+  const [user, setUser] = useState('testUser');
+  const [userText, setUserText] = useState('testUser');
+  const [text, setText] = useState('take a nap');
   const [todoList, setTodoList] = useState<string[]>([]);
+  const [colorList, setColorList] = useState<number[]>([]);
+  const colorNumber = 3;
+  const colors = ['green', 'blue', 'red'];
   useEffect(() => {
-    // HTTPリクエストを送信
-    axios.get(url)
+    getTodos();
+  }, [user]);
+
+  const getTodos = () => {
+    axios.get(url + '/' + user)
       .then(response => {
-        // レスポンスのデータをセット
-        setTodoList(response.data);
+        setTodoList(response.data.todoList);
+        setColorList(response.data.colorList);
       })
       .catch(error => {
         console.error('Error fetching todos:', error);
-      });;
-  }, []);
+      });
+  }
+
   const addTodo = (text: string) => {
     setTodoList([...todoList, text]);
-    axios.post(url, { todo: text })
+    setColorList([...colorList, 0]);
+    axios.post(url + '/' + user, { todo: text })
       .then(response => {
         console.log(response);
       })
@@ -41,41 +51,78 @@ function App() {
 
   const deleteTodo = (index: number) => {
     setTodoList(todoList.filter((_, i) => i !== index));
-    axios.delete(url + '/' + index)
+    setColorList(colorList.filter((_, i) => i !== index));
+    axios.delete(url + '/' + user + '/' + index)
+      .then(response => {
+        console.log(response);
+      })
       .catch(error => {
         console.error('Error deleting todo:', error);
       });
   }
 
+  const changeColor = (index: number) => {
+    setColorList(colorList.map((color, i) => i === index ? (color + 1) % colorNumber : color));
+    axios.put(url + '/' + user + '/' + index)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error('Error changing color:', error);
+      });
+  }
+
   return (
     <>
-      <h1>TODO</h1>
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'white' }}>
-        {todoList.map((todo,index) => 
-        (
-        <ListItem
-          key={index}
-          secondaryAction={
-            <IconButton edge="end" aria-label="comments" 
-            onClick={() => deleteTodo(index)}>
-              <CheckIcon />
-            </IconButton>
-          }
-          disablePadding
-        >
-          <ListItemButton>{todo}</ListItemButton>
-        </ListItem>
-        ))}
-      </List>
+      <TextField
+        label="UserName"
+        value={userText}
+        onChange={(e) => setUserText(e.target.value)}
+        style={{ backgroundColor: 'white' }}
+      />
+      <Button
+        variant="contained"
+        onClick={() => setUser(userText)}
+        style={{ backgroundColor: 'green', height: '60px', color: 'white' }}
+      >
+        Change User
+      </Button>
+      <br />
+
+      <h1><span style={{ color: 'green' }}>{user}</span>'s Todo List</h1>
+      {todoList.length === 0 ? (
+        <h1>You've done all todos!</h1>
+      ) : (
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'white' }}>
+          {todoList.map((todo, index) => (
+            <ListItem
+              key={index}
+              secondaryAction={
+                <IconButton edge="end" aria-label="comments" onClick={() => deleteTodo(index)}>
+                  <CheckIcon />
+                </IconButton>
+              }
+              disablePadding
+              style={{ color: colors[colorList[index]] }}
+            >
+              <ListItemButton onClick={() => changeColor(index)}>
+                {todo}
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      )}
       <br />
       <TextField
         label="Todo"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        style={{ backgroundColor: 'white' }}
       />
       <Button
         variant="contained"
         onClick={() => addTodo(text)}
+        style={{ backgroundColor: 'green', height: '60px', color: 'white' }}
       >
         Add
       </Button>
